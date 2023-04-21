6332c0b4dbbee59a40e1feb2ac26be8ace1bb0a9
LPUWeb <lpuwebdev@lpu.co.in>
2023-04-21 17:07:43 +0530

    form added

--- script/nav.js
+++ script/nav.js
@@ -4,18 +4,18 @@ const nav_html = `
     <img src="assets/yv_latest.png" alt="YV 2023">
 </div>
 <ul class="nav-links">
-    <li><a href="index.html">HOME</a></li>
+    <li><a href="https://youthvibe.lpu.in/index.html">HOME</a></li>
     <li><a target="_blank" href="https://lpu.in">ABOUT LPU</a></li>
-    <li><a href="tour.html">TOUR</a></li>
-    <li><a href="schedule.html">SCHEDULE</a></li>
+    <li><a href="https://youthvibe.lpu.in/tour.html">TOUR</a></li>
+    <li><a href="https://youthvibe.lpu.in/schedule.html">SCHEDULE</a></li>
 
-    <li><a href="events.html">EVENTS</a></li>
-    <li><a href="imun.html">IMUN</a></li>
+    <li><a href="https://youthvibe.lpu.in/events.html">EVENTS</a></li>
+    <li><a href="https://youthvibe.lpu.in/imun.html">IMUN</a></li>
 
-    <li><a href="faqs.html">FAQs</a></li>
-    <li><a href="sponsors.html">SPONSORS</a></li>
-    <li><a href="contactus.html">CONTACT US</a></li>
-    <li><a href="soon.html"><button class="call-to-action"> REGISTER</button></a></li>
+    <li><a href="https://youthvibe.lpu.in/faqs.html">FAQs</a></li>
+    <li><a href="https://youthvibe.lpu.in/sponsors.html">SPONSORS</a></li>
+    <li><a href="https://youthvibe.lpu.in/contactus.html">CONTACT US</a></li>
+    <li><a href="https://schools.lpu.in/youthvibe/register.html" target="_blank"><button class="call-to-action"> REGISTER</button></a></li>
 </ul>
 <div class="burger" id="burger_menu_btn">
     <img src="assets/icons8-menu-rounded-100.png" alt="nav">
@@ -25,17 +25,17 @@ const nav_html = `
         <h1 style="font-size: 3rem;">&times;</h1>
     </div>
     <ul class="nav-slide-links">
-        <li><a href="index.html">HOME</a></li>
+        <li><a href="https://youthvibe.lpu.in/index.html">HOME</a></li>
         <li><a target="_blank" href="https://lpu.in">ABOUT LPU</a></li>
-        <li><a href="tour.html">TOUR</a></li>
-        <li><a href="schedule.html">SCHEDULE</a></li>
+        <li><a href="https://youthvibe.lpu.in/tour.html">TOUR</a></li>
+        <li><a href="https://youthvibe.lpu.in/schedule.html">SCHEDULE</a></li>
 
-        <li><a href="events.html">EVENTS</a></li>
-        <li><a href="imun.html">IMUN</a></li>
+        <li><a href="https://youthvibe.lpu.in/events.html">EVENTS</a></li>
+        <li><a href="https://youthvibe.lpu.in/imun.html">IMUN</a></li>
 
-        <li><a href="faqs.html">FAQs</a></li>
-        <li><a href="sponsors.html">SPONSORS</a></li>
-        <li><a href="contactus.html">CONTACT US</a></li>
+        <li><a href="https://youthvibe.lpu.in/faqs.html">FAQs</a></li>
+        <li><a href="https://youthvibe.lpu.in/sponsors.html">SPONSORS</a></li>
+        <li><a href="https://youthvibe.lpu.in/contactus.html">CONTACT US</a></li>
         <li><a href="https://schools.lpu.in/youthvibe/register.html" target="_blank"><button class="call-to-action"> REGISTER</button></a></li>
     </ul>
 </div>
