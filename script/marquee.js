6268b38b01819ae950ee441b24d29d70411bcabc
LPUWeb <lpuwebdev@lpu.co.in>
2023-04-21 17:13:33 +0530

    form added

--- script/marquee.js
+++ script/marquee.js
@@ -1,35 +1,35 @@
-  $(document).ready(function () {
+  $(document).ready(function() {
 
-    document.getElementById("marquee").innerHTML = `
+      document.getElementById("marquee").innerHTML = `
             <div>YOUTHVIBE 2023 is here!</div>
             <div>LPU's Global Open Fest            </div>
-            <div><a style="text-decoration: none;" href="/soon.html">Register Now</a></div>
+            <div><a style="text-decoration: none;" href="https://schools.lpu.in/youthvibe/register.html" target="_blank">Register Now</a></div>
     `;
 
-  $(".owl-carousel").owlCarousel({  //Basic Speeds
-    slideSpeed: 200,
-    paginationSpeed: 800,
+      $(".owl-carousel").owlCarousel({ //Basic Speeds
+          slideSpeed: 200,
+          paginationSpeed: 800,
 
-    //Autoplay
-    autoPlay: true,
-    goToFirst: true,
-    goToFirstSpeed: 1000,
+          //Autoplay
+          autoPlay: true,
+          goToFirst: true,
+          goToFirstSpeed: 1000,
 
-    // Navigation
-    navigation: false,
-    navigationText: ["prev", "next"],
-    pagination: true,
-    paginationNumbers: true,
+          // Navigation
+          navigation: false,
+          navigationText: ["prev", "next"],
+          pagination: true,
+          paginationNumbers: true,
 
-    // Responsive
-    responsive: true,
-    items: 1,
-    itemsDesktop: [1199, 1],
-    itemsDesktopSmall: [980, 1],
-    itemsTablet: [768, 1],
-    itemsMobile: [479, 1]
+          // Responsive
+          responsive: true,
+          items: 1,
+          itemsDesktop: [1199, 1],
+          itemsDesktopSmall: [980, 1],
+          itemsTablet: [768, 1],
+          itemsMobile: [479, 1]
+      });
+
+
+
   });
-
-
-
-});
